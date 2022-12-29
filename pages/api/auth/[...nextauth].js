4f80f4073d918740e2b1ed4a60ffa292d65f8409
walterrcruz1981 <walterrcruz@yahoo.com>
2022-12-28 17:45:42 -0800

    changed auth credential to github only

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -9,10 +8,7 @@ export const authOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        })
+
         // ...add more providers here
     ],
 }
