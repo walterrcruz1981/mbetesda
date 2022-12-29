9212bef9e753ee8ad946f3b91fe08dc2d805e84c
walterrcruz1981 <walterrcruz@yahoo.com>
2022-12-28 18:01:23 -0800

    removed home live slide

--- app/(home-components)/NavBar.jsx
+++ app/(home-components)/NavBar.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import styles from './homeComponents.module.scss'
-import Image from 'next/image'
 import { MyImage } from '../variables'
 import logo from '../../public/images/logo/logo.png'
 import Links from './Links'
 import Link from 'next/link'
-import liveImage from '../../public/images/assets/live.png'
 
 async function NavBar() {
     return (
         <div className={styles.navbarContainer + ' flex'}>
 
             <Link className={styles.logo} href={'/'}>{MyImage(logo)}</Link>
-
-            <div className={styles.liveWidget}>
-                <Link href={'/sermones'}><Image className={styles.liveBug} src={liveImage} width={75} height={55} alt='lives' /></Link>
-            </div>
             <div className={styles.linksContainer}>
                 <Links />
             </div>
